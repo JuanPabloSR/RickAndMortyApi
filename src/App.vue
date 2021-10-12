271cf2921b7c1454f2096cedbfc62d798ea00362
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-10-12 03:08:12 -0500

    version1.2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<audio src="@/components/sound.mp3" autoplay loop></audio>
+
+ <div class="container">
+   <img src="@/components/titulo.png" >
+  <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListCharacters from '@/components/ListCharacters'
+import FilterByStatus from '@/components/FilterByStatus'
+import FilterByName from '@/components/FilterByName'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #304D74;
+  --background-card: #8DC660;
+  --text-white: #fff;
+  --text-gray: black;
+  --text-orange: #1B7800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 80px;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
